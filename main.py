0dd8ee812d70063b0e348a79e8ee2eff196664f3
Victor Hugo <victorhmds@hotmail.com>
2024-03-01 02:12:25 -0300

    Added collision with the walls

--- main.py
+++ main.py
@@ -7,33 +7,47 @@ def clear_screen():
     system("\033[H")
 
 
+def create_player():
+    global player
+    x, y = FIRST_ROOM_WIDTH // 8, FIRST_ROOM_HEIGHT // 2
+    player = (x, y)
+
+
+def check_collision(position):
+    x, y = position
+    return x in (0, FIRST_ROOM_WIDTH - 1) or y in (0, FIRST_ROOM_HEIGHT - 1)
+
+
+def move_player():
+    global player
+    new_position = tuple(map(add, player, direction))
+
+    if not check_collision(new_position):
+        player = new_position
+
+
 def controls(e):
     global direction, player
     if e.event_type == keyboard.KEY_DOWN:
         if e.name == "w":
             direction = DIRECTIONS["up"]
-            player = tuple(map(add, player, direction))
+            move_player()
         elif e.name == "a":
             direction = DIRECTIONS["left"]
-            player = tuple(map(add, player, direction))
+            move_player()
         elif e.name == "s":
             direction = DIRECTIONS["down"]
-            player = tuple(map(add, player, direction))
+            move_player()
         elif e.name == "d":
             direction = DIRECTIONS["right"]
-            player = tuple(map(add, player, direction))
+            move_player()
 
 
 def create_room(room_width, room_height):
     return [(col, row) for row in range(room_height) for col in range(room_width)]
 
 
-def create_player():
-    x, y = FIRST_ROOM_WIDTH // 8, FIRST_ROOM_HEIGHT // 2
-    return (x, y)
-
-
-def generate_room(room, room_width, room_height):
+def display_room(room, room_width, room_height):
     clear_screen()
     for cell in room:
         if cell[0] in (0, room_width - 1) and cell[1] in (
@@ -53,6 +67,8 @@ def generate_room(room, room_width, room_height):
             print("")
 
 
+keyboard.hook(controls)
+
 FIRST_ROOM_WIDTH = 30
 FIRST_ROOM_HEIGHT = 6
 FIRST_ROOM = create_room(FIRST_ROOM_WIDTH, FIRST_ROOM_HEIGHT)
@@ -60,8 +76,7 @@ FIRST_ROOM = create_room(FIRST_ROOM_WIDTH, FIRST_ROOM_HEIGHT)
 DIRECTIONS = {"left": (-1, 0), "right": (1, 0), "up": (0, -1), "down": (0, 1)}
 direction = None
 
-player = create_player()
-keyboard.hook(controls)
+create_player()
 
 while True:
-    generate_room(FIRST_ROOM, FIRST_ROOM_WIDTH, FIRST_ROOM_HEIGHT)
+    display_room(FIRST_ROOM, FIRST_ROOM_WIDTH, FIRST_ROOM_HEIGHT)
